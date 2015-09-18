88234665b229cdcb38059bb65fd0e509e9532be0
Mickey Burks <mickey@aquameta.com>
2015-09-18 12:59:35 -0700

    Add install script

---
+++ .vim/bundle/install-plugins.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/nanotech/jellybeans.vim
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/groenewege/vim-less
+git clone https://github.com/jistr/vim-nerdtree-tabs
