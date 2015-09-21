47ab671ed39c7b443484a3f93e6162e9a721c9f2
Mickey Burks <brks.mck@gmail.com>
2015-09-21 10:43:27 -0500

    Add Laravel Blade Syntax

---
+++ .vim/bundle/install-plugins.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/nanotech/jellybeans.vim
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/groenewege/vim-less
+git clone https://github.com/jistr/vim-nerdtree-tabs
+git clone git://github.com/xsbeats/vim-blade.git
