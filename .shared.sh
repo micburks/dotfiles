ccff96800441d1c83715993ceba383cc2ae3d26a
Mickey Burks <brks.mck@gmail.com>
2019-08-04 13:35:24 -0700

    Pull request scripts

--- .shared.sh
+++ .shared.sh
@@ -25,7 +25,10 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# source scripts
 source ~/scripts/node_env
+source ~/scripts/pull-requests
 
 # Emacs
 alias emacs='/usr/local/bin/emacs'
