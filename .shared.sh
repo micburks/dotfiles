33b2db5208ce80dc627a73b8605e5870e6d68d89
Mickey Burks <brks.mck@gmail.com>
2019-11-28 17:05:32 -0800

    Utils and aliases

--- .shared.sh
+++ .shared.sh
@@ -1,22 +1,43 @@
 # If mcahine specific setup
 [ -r ~/.machine-specific.sh ] && . ~/.machine-specific.sh
 
+# utils
 alias vi='vim -p'
 alias ls='ls -G'
 alias ll='ls -l'
 alias la='ll -a'
 alias grep='grep --color=auto'
 
+[ "$(alias gcm)" != "" ] && unalias gcm
+[ "$(alias gl)" != "" ] && unalias gl
+[ "$(alias ga)" != "" ] && unalias ga
+
 # git
-alias g='git'
-alias gs='git status'
-alias gcm='git commit -m'
-alias gch='git checkout'
-alias gpu='git pull'
-alias gst='git stash'
+function g () { git $@ }
+function gs () { git status $@ }
+function ga () { git add $@ }
+function gd () { git diff $@ }
+function gb () { git branch $@ }
+function gl () { git log $@ }
+function gch () { git checkout $@ }
+function gcm () { git commit -m $@ }
+function gps () { git push $@ }
+function gpl () { git pull $@ }
+
+function gchi () {
+  i gb git checkout
+}
+
+# util
+function i () {
+  local get=$1;
+  shift;
+  $get | fzf | xargs $@
+}
 
 function cl () {
-  half=stty size | awk '{print int($1/2)-1;}'
+  half=$(stty size | awk '{print int($1/2)-1;}')
+  echo $half
   for i in `seq ${half}`;
     do echo ''
   done
@@ -46,17 +67,24 @@ function eachf () {
 if [ $(uname -s) = 'Darwin' ]; then
   # Postgres config - Mac install of Postgres.App
   #export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/{VERSION}/bin
-  #export PGDATA=/Users/mickey/Library/Application\ Support/Postgres/var-{VERSION}
-  #alias pglog='tail -f /Users/mickey/Library/Application\ Support/Postgres/var-{VERSION}/postgresql.log'
+  #export PGDATA=/Users/mburks/Library/Application\ Support/Postgres/var-{VERSION}
+  #alias pglog='tail -f /Users/mburks/Library/Application\ Support/Postgres/var-{VERSION}/postgresql.log'
 
   export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin/
-  export PGDATA=/Users/mickey/Library/Application\ Support/Postgres/var-10
-  alias pglog="tail -f /Users/mickey/Library/Application\ Support/Postgres/var-10/postgresql.log"
+  export PGDATA=/Users/mburks/Library/Application\ Support/Postgres/var-10
+  alias pglog="tail -f /Users/mburks/Library/Application\ Support/Postgres/var-10/postgresql.log"
 fi
 
+# nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# node
+alias n8='nvm exec 8'
+alias n10='nvm exec 10'
+alias n12='nvm exec 12'
+alias n13='nvm exec 13'
 
 # source scripts
 # source ~/scripts/node_env
@@ -102,10 +130,13 @@ alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall 
 # fuzzy find with colors
 alias fzp="fzf --preview 'bat --style=numbers --color=always {}'"
 alias fzo='vi $(fzp)'
+
+# fzf options
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND='fd --type f'
 
 # user bins
 export PATH="~/bin:$PATH"
 
 # rust bins
-export PATH="~/.cargo/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
