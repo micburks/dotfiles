8e04d43fed944df17771d3a0e3f2e846ca8ee85e
Mickey Burks <brks.mck@gmail.com>
2020-07-13 11:29:57 -0700

    Cleanup help

--- .shared.sh
+++ .shared.sh
@@ -1,3 +1,8 @@
+### help        - print all help comments
+help() {
+  awk -F'###' '/^###/ { print $2 }' ~/.shared.sh
+}
+
 # If machine specific setup
 [ -r ~/.machine-specific.sh ] && . ~/.machine-specific.sh
 
@@ -32,7 +37,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-### nX - run node using specific version (8/10/12/14)
+### nX          - run node using specific version (8/10/12/14)
 alias n8='nvm exec 8'
 alias n10='nvm exec 10'
 alias n12='nvm exec 12'
@@ -45,25 +50,19 @@ alias n14='nvm exec 14'
 # utils
 #
 # -------------------------------
-### help - print all help comments
-help() {
-  awk -F'###' '/^###/ { print $2 }' ~/.shared.sh
-}
-
+### v/vi/vim    - vim
 export EDITOR=vim
+alias v='vim -p'
+alias vi='vim -p'
 
 # ls colors
 eval $(dircolors ~/.nix-profile/share/LS_COLORS)
 alias ls='ls --color=auto -F'
 
-### v/vi/vim
-alias v='vim -p'
-alias vi='vim -p'
-
-### vn - switch to compatible node/yarn versions
+### vn          - switch to compatible node/yarn versions
 alias vn='virtualnode'
 
-### uu - cd to root parent `git` directory
+### uu          - cd to root parent `git` directory
 function uu() {
   # go to root directory of current git repo
   if [ -s ".git" ]; then
@@ -74,14 +73,14 @@ function uu() {
   fi
 }
 
-### i - interactively pipe first command to second command
+### i           - interactively pipe first command to second command
 function i () {
   local get=$1;
   shift;
   $get | fzf | xargs $@
 }
 
-### cl - clear half of terminal
+### cl          - clear half of terminal
 function cl () {
   half=$(stty size | awk '{print int($1/2)-1;}')
   echo $half
@@ -91,17 +90,17 @@ function cl () {
   tput cup ${half} 0 && tput ed
 }
 
-### lb - open daily logbook
+### lb          - open daily logbook
 function lb () {
   vim ~/logbook/$(date '+%Y-%m-%d').md
 }
 
-### copy FILE - copy file contents
+### copy FILE   - copy file contents
 function copy() {
   cat $1 | pbcopy
 }
 
-### paste FILE - destructively paste clipboard contents to file
+### paste FILE  - destructively paste clipboard contents to file
 function paste() {
   pbpaste > $1
 }
@@ -115,6 +114,7 @@ if [ $(uname -s) = 'Darwin' ]; then
 
   export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin/
   export PGDATA=/Users/MICKEY/Library/Application\ Support/Postgres/var-10
+### pglog       - tail postgres logs
   alias pglog="tail -f /Users/MICKEY/Library/Application\ Support/Postgres/var-10/postgresql.log"
 fi
 
@@ -123,14 +123,14 @@ source ~/scripts/virtualnode.sh
 source ~/scripts/pull-requests
 
 # Edit and source bash in one command - useful for testing commands as you write
-### esh - edit and source shared shell configuration
+### esh         - edit and source shared shell configuration
 alias esh="vi ~/.shared.sh && source ~/.shared.sh"
 
 # user scripts
 export PATH="$HOME/bin:$HOME/machine-specific-scripts:$HOME/scripts:$PATH"
 
 # show hidden files in finder
-### showFiles - fix mac finder for hidden files
+### showFiles   - fix mac finder for hidden files
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 
 
@@ -143,10 +143,10 @@ alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall 
 # Emacs
 alias emacs='/usr/local/bin/emacs'
 
-### json - pretty display json dump from stdin
+### json        - pretty display json dump from stdin
 alias json='python -m json.tool'
 
-### r/ranger - ranger file explorer
+### r/ranger    - ranger file explorer
 alias ranger="~/bin/ranger/ranger.py"
 alias r="ranger"
 
@@ -158,20 +158,20 @@ export YVM_DIR=/Users/MICKEY/.yvm
 [ -r $YVM_DIR/yvm.sh ] && . $YVM_DIR/yvm.sh
 
 # autojump
-### j - autojump
+### j           - autojump
 [[ -s /Users/MICKEY/.autojump/etc/profile.d/autojump.sh ]] && source /Users/MICKEY/.autojump/etc/profile.d/autojump.sh
 
 # fzf options
-### fzf - fuzzy find files
+### fzf         - fuzzy find files
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND='fd --type f'
 
 # fuzzy find with colors
-### fzp - fuzzy preview files
+### fzp         - fuzzy preview files
 alias fzp="fzf --preview 'bat --style=numbers --color=always {}'"
-### fzo - fuzzy open files
+### fzo         - fuzzy open files
 alias fzo='vi $(fzp)'
 
 # nix
-### , - run tool from nix-pkg without installing
+### ,           - run tool from nix-pkg without installing
 if [ -e /Users/MICKEY/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/MICKEY/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
