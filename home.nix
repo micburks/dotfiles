b17c30a718692315527a7681723450046d45850d
Mickey Burks <mburks@uber.com>
2021-07-14 12:40:07 -0500

    Add comma

--- home.nix
+++ home.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
 
+let
+  comma = import ( pkgs.fetchFromGitHub {
+      owner = "Shopify";
+      repo = "comma";
+      rev = "4a62ec17e20ce0e738a8e5126b4298a73903b468";
+      sha256 = "0n5a3rnv9qnnsrl76kpi6dmaxmwj1mpdd2g0b4n1wfimqfaz6gi1";
+  }) {};
+
+in
+
 {
   programs.home-manager.enable = true;
 
@@ -16,6 +26,7 @@
     alacritty
     autojump
     bat
+    comma
     coreutils
     delta
     exa
