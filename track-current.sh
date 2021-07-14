657a032c6fb93eb8c27ca8b6598148c4d6f52d57
Mickey Burks <mburks@uber.com>
2021-07-14 14:25:21 -0500

    Stop tracking old files

--- track-current.sh
+++ track-current.sh
@@ -3,7 +3,7 @@ USER="MICKEY"
 
 #cat ~/.bash_profile | sed "s/$(whoami)/$USER/g" > .bash_profile
 cat ~/.gitconfig | sed "s/$(whoami)/$USER/g" > .gitconfig
-cat ~/.shared.sh | sed "s/$(whoami)/$USER/g" > .shared.sh
+#cat ~/.shared.sh | sed "s/$(whoami)/$USER/g" > .shared.sh
 #cat ~/.vimrc | sed "s/$(whoami)/$USER/g" > .vimrc
 #cat ~/.zshrc | sed "s/$(whoami)/$USER/g" > .zshrc
 #cat ~/.config/vmd | sed "s/$(whoami)/$USER/g" > vmd
@@ -12,12 +12,12 @@ cat ~/.shared.sh | sed "s/$(whoami)/$USER/g" > .shared.sh
 # cp ~/.vim/bundle/install-plugins.bash .vim/bundle/
 cp ~/.gitignore .
 cp ~/.tmux.conf .
-cp ~/.psqlrc .
-rsync -av --exclude='node_modules' ~/scripts/ ./scripts
+#cp ~/.psqlrc .
+#rsync -av --exclude='node_modules' ~/scripts/ ./scripts
 
 
 # nix
-cp ~/.config/nixpkgs/home.nix .
+#cp ~/.config/nixpkgs/home.nix .
 
 # nvim
-cat ~/.nvimrc | sed "s/$(whoami)/$USER/g" > .nvimrc
+#cat ~/.nvimrc | sed "s/$(whoami)/$USER/g" > .nvimrc
