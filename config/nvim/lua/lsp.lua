82cd69ad7f3042768947d33f6ae9f7add4e24af9
Mickey Burks <brks.mck@gmail.com>
2022-01-09 12:01:33 -0600

    Fix autocomplete and add snippets

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,14 +1,93 @@
 -- npm i -g bash-language-server graphql-language-service-cli flow-bin typescript-language-server vscode-langservers-extracted vim-language-server eslint_d @tailwindcss/language-server
 
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 -- autocomplete packages
--- these are not available via nix right now
+-- these are not updated in nix right now
 local use = require('packer').use
 require('packer').startup(function()
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/vim-vsnip'
+  use 'hrsh7th/vim-vsnip-integ'
+  use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'hrsh7th/cmp-nvim-lua' -- LSP source for neovim lua api
+  use 'lukas-reineke/cmp-rg' -- LSP source for ripgrep
+  use 'hrsh7th/cmp-buffer' -- LSP source for buffer
+  use 'rafamadriz/friendly-snippets'
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
 end)
+
+local cmp = require('cmp')
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    -- { name = 'luasnip' },
+    { name = 'nvim_lua' },
+    { name = 'rg' }
+  }, {
+    { name = 'buffer' }
+  })
+}
+
+---
+--- ... autocomplete ...
+--- <C-p>         - cmp.mapping.select_prev_item
+--- <C-n>         - cmp.mapping.select_next_item
+--- <C-d>         - cmp.mapping.scroll_docs back
+--- <C-f>         - cmp.mapping.scroll_docs forward
+--- <C-Space>     - cmp.mapping.complete
+--- <C-e>         - cmp.mapping.close
+--- <CR>          - cmp.mapping.confirm
+--- <Tab>         - select_next or expand_or_jump
+--- <S-Tab>       - select_prev or jump back
+
 
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 for type, icon in pairs(signs) do
@@ -187,65 +266,3 @@ nvim_lsp.efm.setup {
     "typescriptreact"
   },
 }
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
----
---- ... autocomplete ...
---- <C-p>         - cmp.mapping.select_prev_item
---- <C-n>         - cmp.mapping.select_next_item
---- <C-d>         - cmp.mapping.scroll_docs back
---- <C-f>         - cmp.mapping.scroll_docs forward
---- <C-Space>     - cmp.mapping.complete
---- <C-e>         - cmp.mapping.close
---- <CR>          - cmp.mapping.confirm
---- <Tab>         - select_next or expand_or_jump
---- <S-Tab>       - select_prev or jump back
