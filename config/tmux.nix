183e45342b8bc7782a9fcb2c7be989632bb1f5e9
Mickey Burks <brks.mck@gmail.com>
2022-02-09 16:23:27 -0600

    Tmux plugins and config

--- config/tmux.nix
+++ config/tmux.nix
@@ -8,5 +8,26 @@
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "tmux-256color";
     extraConfig = builtins.readFile ~/.config/nixpkgs/config/tmux.conf;
+    newSession = true;
+    plugins = with pkgs.tmuxPlugins; [
+    {
+      plugin = resurrect;
+      extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+    }
+    {
+      plugin = continuum;
+      extraConfig = ''
+        set -g @continuum-restore 'on'
+        set -g @continuum-save-interval '60' # minutes
+        '';
+    }
+    {
+      plugin = extrakto;
+      extraConfig = ''
+        set -g @extrakto_copy_key "tab"      # use tab to copy to clipboard
+        set -g @extrakto_insert_key "enter"  # use enter to insert selection
+        '';
+    }
+    ];
   };
 }
