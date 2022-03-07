bf845063653895e25432d5a72de206b9aaf5d62c
Mickey Burks <brks.mck@gmail.com>
2022-03-07 11:13:32 -0600

    Remove comma

--- home.nix
+++ home.nix
@@ -1,12 +1,6 @@
 { config, pkgs, email, ... }:
 
 let
-  comma = import ( pkgs.fetchFromGitHub {
-      owner = "Shopify";
-      repo = "comma";
-      rev = "4a62ec17e20ce0e738a8e5126b4298a73903b468";
-      sha256 = "0n5a3rnv9qnnsrl76kpi6dmaxmwj1mpdd2g0b4n1wfimqfaz6gi1";
-  }) {};
   git = import ./config/git.nix { inherit config pkgs email; };
 in
 {
@@ -30,7 +24,6 @@ in
     alacritty
     autojump
     bat
-    comma
     coreutils
     delta
     direnv
