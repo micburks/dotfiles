e757d6985488caef194d93ff7d1eb32c607b3097
Mickey Burks <brks.mck@gmail.com>
2022-03-11 10:37:47 -0600

    Delete unused lua module

--- config/nvim/lua/modules/focus.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.focus = function()
-  require('focus').focus_max_or_equal()
-  local key = nvim_replace_termcodes("<C-w>=", v:true, v:false, v:true)
-  nvim_feedkeys(key, 'n', v:true)
-end
-
-return M
