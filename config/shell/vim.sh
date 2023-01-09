3746d7cc88938de726390fad3c0ed51a76ac359c
Mickey Burks <brks.mck@gmail.com>
2023-01-09 11:53:54 -0600

    Have nvim without args open unmerged files

--- config/shell/vim.sh
+++ config/shell/vim.sh
@@ -10,12 +10,18 @@ export TERM=xterm-256color
 export XDG_CONFIG_HOME=$HOME/.config
 alias v='vim'
 alias vi='vim'
+alias nvim='vim'
 function vim() {
   local original_args=("$@")
   local vim_args=()
   local filtered_vim_args=()
   local new=0
   local fail=0
+
+  if [[ "$@" == "" ]]; then
+    vim_unmerged
+    return
+  fi
 
   # check for "--new"
   for arg in "${original_args[@]}"; do
@@ -28,9 +34,9 @@ function vim() {
 
   if (( new )); then
     # if "--new", let vim create new files
-    nvim -p "${vim_args[@]}"
+    command nvim -p "${vim_args[@]}"
   else
-    # if not "--new", filter non-existant files
+    # if not "--new", filter non-existent files
     for file in "${vim_args[@]}"; do
       # ignore options
       [[ $file = -* ]] && continue
@@ -38,9 +44,10 @@ function vim() {
       if ! [[ -e $file ]]; then
         printf '[vim wrapper] ignored argument "%s" - no such file or directory\n' "$file" >&2
         fail=1
-      elif [[ -d $file ]]; then
-        printf '[vim wrapper] ignored argument "%s" - refusing to open directory\n' "$file" >&2
-        fail=1
+      # after switching to nvim, there is a much better behavior with opening a directory
+      #elif [[ -d $file ]]; then
+        #printf '[vim wrapper] ignored argument "%s" - refusing to open directory\n' "$file" >&2
+        #fail=1
       else
         filtered_vim_args+=( $file )
       fi
@@ -49,16 +56,23 @@ function vim() {
     if (( fail )); then
       echo -e "[vim wrapper] filtered arguments - force default behavior by using \"--\e[4mn\e[0mew\"\n"
       echo "  vim $@ -n"
+      echo ""
     fi
 
     if [[ "$filtered_vim_args" == "" ]]; then
-      if (( fail )); then
-        # extra space after previous log
-        echo ""
-      fi
       echo "[vim wrapper] no files to open"
     else
-      nvim -p "${filtered_vim_args[@]}"
+      command nvim -p "${filtered_vim_args[@]}"
     fi
+  fi
+}
+
+# open unmerged files if they exist
+function vim_unmerged() {
+  local unmerged="$(git diff --name-only --diff-filter=U)"
+  if [[ "$unmerged" != "" ]]; then
+    echo $unmerged | xargs command nvim -p
+  else
+    echo "[vim_unmerged] no unmerged files to open"
   fi
 }
